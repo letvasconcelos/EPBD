c68f124281d008b8e6d769e4a3c630031354ee63
Leticia Vasconcelos <lcvasconcelos80gmail.com>
2024-11-29 23:19:58 -0300

    jar gerado. Para executar java -jar build/libs/meu-projeto.jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,18 +15,25 @@ repositories {
 }
 
 dependencies {
-    // Dependências Spring Boot
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("com.microsoft.sqlserver:mssql-jdbc:12.2.0.jre11") // Verifique se esta é a versão que você deseja
+    implementation("com.microsoft.sqlserver:mssql-jdbc:12.2.0.jre11")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.microsoft.sqlserver:mssql-jdbc:12.2.0.jre11")
-    implementation("org.jetbrains.exposed:exposed-core:0.41.1") // Biblioteca Exposed Core
-    implementation("org.jetbrains.exposed:exposed-dao:0.41.1") // Biblioteca Exposed DAO
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1") // Biblioteca Exposed para JDBC
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.jetbrains.exposed:exposed-core:0.41.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17" // Define o target da JVM
+    }
+    tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootJar> {
+        archiveFileName.set("EPBD.jar") // Nome final do arquivo JAR
+    }
 
 }
